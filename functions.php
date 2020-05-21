1d5bbdb05d53e746e6d89db92f5a2aaa67e25d07
colinah <halecolin1@gmail.com>
2020-05-21 15:17:02 -0600

    Updates

--- functions.php
+++ functions.php
@@ -6,10 +6,13 @@ function universiry_files() {
     wp_enqueue_style('university_main_styles',get_stylesheet_uri(),NULL, microtime());
 }
 
+add_action('wp_enqueue_scripts', 'universiry_files');
+
 function university_features() {
+    register_nav_menu('headerMenuLocation', "Header Menu Location");
+    register_nav_menu('FooterExploreLocation', "Footer Explore Location");
+    register_nav_menu('FooterLearnLocation', "Footer Learn Location");
     add_theme_support('title-tag');
 }
-
-add_action('wp_enqueue_scripts', 'universiry_files');
 
 add_action('after_setup_theme', 'university_features');
