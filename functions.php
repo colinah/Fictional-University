3f5d1ab1b4953f370a5c7c3bf32e5f7fe1482c05
colinah <halecolin1@gmail.com>
2020-06-03 17:50:18 -0600

    Adding relations backend

--- functions.php
+++ functions.php
@@ -1,19 +1,26 @@
 <?php
 
 function university_adjust_queries($query) {
-    $today = date('Ymd');
-    if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
-    $query->set('meta_key', 'event_date');
-    $query->set('orderby', 'meta_value_num');
+  if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+    $query->set('orderby','title');
     $query->set('order', 'ASC');
-    $query->set('meta_query', array(
-        array(
-        'key' => 'event_date',
-        'compare' =>  '<=',
-        'value' => date("Ymd"),
-        'type' => 'numeric'
-      )));
-    }
+    $query->set('post_per_page', -1);
+
+  }
+
+  $today = date('Ymd');
+  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+  $query->set('meta_key', 'event_date');
+  $query->set('orderby', 'meta_value_num');
+  $query->set('order', 'ASC');
+  $query->set('meta_query', array(
+      array(
+      'key' => 'event_date',
+      'compare' =>  '<=',
+      'value' => date("Ymd"),
+      'type' => 'numeric'
+    )));
+  }
 }
 
 add_action('pre_get_posts','university_adjust_queries');
